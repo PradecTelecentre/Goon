981f7c03fd8896dd887273f1fbc108914dc1e8ab
Abdoul aziz Hamayadji <abdoulazizhamayadji95@gmail.com>
2020-01-15 19:12:30 +0100

    nouvelle

--- free-services.php
+++ free-services.php
@@ -43,7 +43,7 @@
 
 
                     <div class="col-md-3 mb-4 col-sm-6 col-lg-3 mobile-cover">
-                        <a href="nouvelle/" rel="bookmark">
+                        <a href="information/" rel="bookmark">
                             <div class="p-2 bg-goon style-div-md-4">
                                 <img src="<?= get_template_directory_uri() . '/src/img/info.png' ?>" alt="">
 
