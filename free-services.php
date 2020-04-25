6f0e893feddcc63936a92127a3a13d8b7d6d7ed5
claudelkros <desiretaira@gmail.com>
2020-04-26 00:01:28 +0100

    second 2 commit for market link fixe

--- free-services.php
+++ free-services.php
@@ -94,7 +94,7 @@
 
 
                     <div class="col-md-3 mb-4 col-sm-6 col-lg-3 mobile-cover">
-                        <a href="http://telecentrecameroon.ovh:5000/" rel="bookmark">
+                        <a href="http://telecentrecameroon.ovh/marches" rel="bookmark">
                             <div class="p-2 bg-white style-div-md-4">
                                 <img src="<?= get_template_directory_uri() . '/src/img/v2/marche.png' ?>" alt="">
                                 <?php if (strpos(home_url('/'), 'lang=en') !== false) { ?>
