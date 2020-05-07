0a6810f3a9fd9d724b0536fb0de7716f16e8d0f9
NoelaT <tenkunoela@gmail.com>
2020-05-07 12:59:28 +0100

    Template for services page

--- services-template.php
+++ services-template.php
@@ -1,12 +1,8 @@
 <?php
 
 /**
- * The main template file
- *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
+ * Template Name: Services layout
+ * This template is for the home page to aid with translations.
  *
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
  *
