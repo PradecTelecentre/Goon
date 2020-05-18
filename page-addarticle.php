f899e0f5ae5ae7da77c4678c70f8ee7dfaaf0b08
Abdoul aziz Hamayadji <abdoulazizhamayadji95@gmail.com>
2020-05-18 12:22:50 +0100

    draft item in front end

--- page-addarticle.php
+++ page-addarticle.php
@@ -20,11 +20,11 @@ get_header();
                                 'html_after_fields' => '',
                                 'submit_value' => 'Submit Post',
                                 'updated_messag' => 'Saved!',
-                                'field_el'=>'div',
+                                // 'field_el'=>'div',
                                 'uploader' => 'basic',
                                 'new_post' => array(
                                         'post_type'	=> 'marche', // You can change to a custom post type
-                                        'post_status'	=> 'publish',  // draft
+                                        'post_status'	=> 'draft',  // publish 
                                         'article_title'    =>  wp_strip_all_tags($_POST['acf']['field_5ec19d5230e83']), // Post Title ACF field key
                                         'article_quantity'    =>  wp_strip_all_tags($_POST['acf']['field_5ea4ae24ec531']), // Post Title ACF field key
                                         'article_description'    =>  wp_strip_all_tags($_POST['acf']['field_5ea4aeb0ec532']), // Post Title ACF field key
