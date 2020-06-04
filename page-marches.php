f658da9b042ee29972829fc525d4382c6f2f6e92
Abdoul aziz Hamayadji <abdoulazizhamayadji95@gmail.com>
2020-06-04 15:52:47 +0100

    pending article

--- page-marches.php
+++ page-marches.php
@@ -57,15 +57,8 @@ get_header();
                                                 $data->the_post(); ?>
                                                 <div class="row d-flex flex-row"> 
                                                         <div class="col">
-                                                            <div class="champ-commun" style="">   
-                                                                <?php  
-                                                                if(empty(the_title())) { 
-                                                                    echo the_field('article_title');
-                                                                }else{
-                                                                    the_title(); 
-                                                                }   
-                                                                ?> 
-                                                                </div>
+                                                            <div class="champ-commun" style="">   <?php   the_title();   ?> </div>
+                                                            <div class="champ-commun" style="">  <?php the_field('article_quantity')   ?> </div>
                                                             <div class="text-muted" style="font-size: 20px;"><?php the_field('article_quantity') ?> disponible </div>
                                                         </div>
                                                         <div class="col">
