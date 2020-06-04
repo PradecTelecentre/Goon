18febd8da266f729cb0d76c101f21ba9849c0ee2
Abdoul aziz Hamayadji <abdoulazizhamayadji95@gmail.com>
2020-06-04 15:13:22 +0100

    pending article

--- 404.php
+++ 404.php
@@ -24,30 +24,30 @@ get_header();
 					<?php
 					get_search_form();
 
-					the_widget( 'WP_Widget_Recent_Posts' );
+					// the_widget( 'WP_Widget_Recent_Posts' );
 					?>
 
-					<div class="widget widget_categories">
+					<!-- <div class="widget widget_categories">
 						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'telecenter' ); ?></h2>
 						<ul>
 							<?php
-							wp_list_categories( array(
-								'orderby'    => 'count',
-								'order'      => 'DESC',
-								'show_count' => 1,
-								'title_li'   => '',
-								'number'     => 10,
-							) );
+							// wp_list_categories( array(
+							// 	'orderby'    => 'count',
+							// 	'order'      => 'DESC',
+							// 	'show_count' => 1,
+							// 	'title_li'   => '',
+							// 	'number'     => 10,
+							// ) );
 							?>
 						</ul>
-					</div><!-- .widget -->
+					</div>.widget -->
 
 					<?php
 					/* translators: %1$s: smiley */
-					$telecenter_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'telecenter' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$telecenter_archive_content" );
+					// $telecenter_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'telecenter' ), convert_smilies( ':)' ) ) . '</p>';
+					// the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$telecenter_archive_content" );
 
-					the_widget( 'WP_Widget_Tag_Cloud' );
+					// the_widget( 'WP_Widget_Tag_Cloud' );
 					?>
 
 				</div><!-- .page-content -->
