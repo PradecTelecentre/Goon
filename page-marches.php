414cecd804658feec1aa8b26e347a950eb5ea05e
Abdoul aziz Hamayadji <abdoulazizhamayadji95@gmail.com>
2020-06-05 11:09:07 +0100

    pending article

--- page-marches.php
+++ page-marches.php
@@ -88,7 +88,7 @@ get_header();
                             
                                     echo paginate_links(array(
                                         'base' => get_pagenum_link(1) . '%_%',
-                                        'format' => '/page/%#%',
+                                        'format' => '?page=%#%',
                                         'current' => $current_page,
                                         'total' => $total_pages,
                                         'prev_text'    => __('« prev'),
