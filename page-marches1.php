8449111f48cb09eaf3920f214e5e1ae7e4142f46
Abdoul aziz Hamayadji <abdoulazizhamayadji95@gmail.com>
2020-06-29 14:02:17 +0100

    mise en place du corps html

--- page-marches1.php
+++ page-marches1.php
@@ -5,9 +5,10 @@ Template Name: marche 1.1 aziz
 acf_form_head();
 get_header();
 ?>
-
-
-    <h1> new market pages </h1>
+    <div clsss="" style="margin-top:53px">
+        <h1> new market pages </h1>
+    </div>
+    
 
 <?php 
 get_footer();
