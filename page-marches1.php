1255671d3c6af333e90f3e55903350b2b457a62f
Abdoul aziz Hamayadji <abdoulazizhamayadji95@gmail.com>
2020-06-29 14:08:24 +0100

    new market design

--- page-marches1.php
+++ page-marches1.php
@@ -5,10 +5,27 @@ Template Name: marche 1.1 aziz
 acf_form_head();
 get_header();
 ?>
-    <div clsss="" style="margin-top:53px">
+    <div class="" style="margin-top:53px; bagckground-color:red">
         <h1> new market pages </h1>
     </div>
-    
 
+    <div class="mainpage" style="bagckground-color:blue"> 
+        <div class="row">
+            <div class="col-12">
+                principale
+            </div>
+        </div>
+
+
+        <div class="row">
+            <div class="col-3"> h1 </div>
+            <div class="col-3"> h1 </div>
+            <div class="col-3"> h1 </div>
+            <div class="col-3"> h1 </div>
+        </div>
+            
+    
+    </div>
+    
 <?php 
 get_footer();
