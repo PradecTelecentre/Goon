a780df660636f95fd0f75ae2ed5464b7c3888209
Abdoul aziz Hamayadji <abdoulazizhamayadji95@gmail.com>
2020-07-01 21:33:22 +0100

    single pages for market

--- page-marches1.php
+++ page-marches1.php
@@ -6,8 +6,9 @@ acf_form_head();
 get_header();
 ?>
     <div class="" style="margin-top: 53px; position:relative; background-color: darkorange; height: 375px; border-radius: 1px 1px 100px 100px; padding-top: 30px">
-    <marquee> Les articles publiés dans GOON sont supprimés après un délais de 7 jours.
-        Notez Bien tout contenu inapproprié est immédiatement supprimé de la plateforme  </marquee>
+        <marquee> Les articles publiés dans GOON sont supprimés après un délais de 7 jours.
+            Notez Bien tout contenu inapproprié est immédiatement supprimé de la plateforme  
+        </marquee>
         <div class="image text-center" style="opacity:0.4; position: relative; text-align:center; color:white;">
             <img class="" src="<?= get_template_directory_uri() . '/src/img/marche3.jpg' ?>" alt="" style="height:260px; ">
             <div class="" style="position: absolute; top:30%; left: 50%;  transform: translate(-50%, -50%); font-size: 44px; color: #FFFFFF; font-weight: 600; font-family: 'Roboto Slab', Sans-serif !important; text-transform: uppercase;" >MARCHÉ</div>
@@ -16,11 +17,7 @@ get_header();
     </div>
 
     <div class="container mainpage" style="background-color: transparent;  margin-top: -65px;"> 
-        <!-- <div class="row" type=""hidden>
-            <div class="col-12 text-center" style="background-color: transparent">
-                <h1> principale </h1>
-            </div>
-        </div> -->
+        
     
         <div class="text-center container row">
             <?php
