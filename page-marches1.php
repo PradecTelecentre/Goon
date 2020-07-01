589c0d3c8a569ca7ace6f71a5aebc0b153c2a7ad
Abdoul aziz Hamayadji <abdoulazizhamayadji95@gmail.com>
2020-07-01 09:37:44 +0100

    new market design

--- page-marches1.php
+++ page-marches1.php
@@ -22,61 +22,18 @@ get_header();
 
         <div class="row">
             <div class="col-3"> 
-
                 <!-- Card -->
                 <div class="card">
-
-                <div class="view zoom overlay">
-                    <img class="img-fluid w-100" src="https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/13a.jpg" alt="Sample">
-                    <h4 class="mb-0"><span class="badge badge-primary badge-pill badge-news">Sale</span></h4>
-                    <a href="#!">
-                    <div class="mask">
-                        <img class="img-fluid w-100"
-                        src="https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/13.jpg">
-                        <div class="mask rgba-black-slight"></div>
+                <img class="img-fluid w-100" src="https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/13a.jpg" alt="Sample">
+                    <div class="card-body">
+                        <h5 class="text-center"> Artice x </h5>
+                        <p class="small text-muted text-uppercase text-center"> 01 disponible</p>
+                        <hr>
+                        <h6 class="mb-3">
+                            <span class="text-danger"> 100000 FCFA</span>
+                        </h6>
+                        <a href="#" class="btn btn-light btn-sm mr-1 mb-2"> DETAILS</a>
                     </div>
-                    </a>
-                </div>
-
-                <div class="card-body text-center">
-
-                    <h5>Fantasy T-shirt</h5>
-                    <p class="small text-muted text-uppercase mb-2">Shirts</p>
-                    <ul class="rating">
-                    <li>
-                        <i class="fas fa-star fa-sm text-primary"></i>
-                    </li>
-                    <li>
-                        <i class="fas fa-star fa-sm text-primary"></i>
-                    </li>
-                    <li>
-                        <i class="fas fa-star fa-sm text-primary"></i>
-                    </li>
-                    <li>
-                        <i class="fas fa-star fa-sm text-primary"></i>
-                    </li>
-                    <li>
-                        <i class="far fa-star fa-sm text-primary"></i>
-                    </li>
-                    </ul>
-                    <hr>
-                    <h6 class="mb-3">
-                    <span class="text-danger mr-1">$12.99</span>
-                    <span class="text-grey"><s>$36.99</s></span>
-                    </h6>
-
-                    <button type="button" class="btn btn-primary btn-sm mr-1 mb-2">
-                    <i class="fas fa-shopping-cart pr-2"></i>Add to cart
-                    </button>
-                    <button type="button" class="btn btn-light btn-sm mr-1 mb-2">
-                    <i class="fas fa-info-circle pr-2"></i>Details
-                    </button>
-                    <button type="button" class="btn btn-danger btn-sm px-3 mb-2 material-tooltip-main" data-toggle="tooltip" data-placement="top" title="Add to wishlist">
-                    <i class="far fa-heart"></i>
-                    </button>
-
-                </div>
-
                 </div>
                 <!-- Card -->
             </div>
