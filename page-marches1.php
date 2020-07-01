2ebc48b580987c936ee8a4aafa8d2a3e60687523
Abdoul aziz Hamayadji <abdoulazizhamayadji95@gmail.com>
2020-07-01 10:13:47 +0100

    new market design

--- page-marches1.php
+++ page-marches1.php
@@ -9,40 +9,90 @@ get_header();
         <div class="image text-center">
             <img src="<?= get_template_directory_uri() . '/src/img/market1.jpg' ?>" alt="" >
         </div>
-        <h1 class="text-center"> new market pages </h1>
+        <marquee> Les articles publiés dans GOON sont supprimés après un délais de 7 jours.
+        Notez Bien tout contenu inapproprié est immédiatement supprimé de la plateforme  </marquee>
     </div>
 
-    <div class="mainpage" style="    background-color: blue; position: absolute; width: 100%; margin-top: -65px;"> 
+    <div class="container mainpage" style="background-color: transparent;  margin-top: -65px;"> 
         <div class="row">
-            <div class="col-12">
-                principale
+            <div class="col-12 text-center" style="background-color: blue">
+                <h1> principale </h1>
             </div>
         </div>
 
 
-        <div class="row">
+        <div class="text-center container row">
             <div class="col-3"> 
                 <!-- Card -->
                 <div class="card">
-                <img class="img-fluid w-100" src="https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/13a.jpg" alt="Sample">
+                    <img class="img w-100" src="https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/13a.jpg" alt="Sample">
                     <div class="card-body">
                         <h5 class="text-center"> Artice x </h5>
                         <p class="small text-muted text-uppercase text-center"> 01 disponible</p>
                         <hr>
-                        <h6 class="mb-3">
-                            <span class="text-danger"> 100000 FCFA</span>
+                        <h6 class="mb-3 text-center">
+                            <span class="text-danger "> 100000 FCFA</span>
                         </h6>
-                        <a href="#" class="btn btn-light btn-sm mr-1 mb-2"> DETAILS</a>
+                        <a href="#" class="btn btn-light btn-small mr-1 mb-2"> DETAILS</a>
                     </div>
                 </div>
                 <!-- Card -->
             </div>
-            <div class="col-3"> h1 </div>
-            <div class="col-3"> h1 </div>
-            <div class="col-3"> h1 </div>
+            <div class="col-3"> 
+                <!-- Card -->
+                <div class="card">
+                    <img class="img w-100" src="https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/13a.jpg" alt="Sample">
+                    <div class="card-body">
+                        <h5 class="text-center"> Artice x </h5>
+                        <p class="small text-muted text-uppercase text-center"> 01 disponible</p>
+                        <hr>
+                        <h6 class="mb-3 text-center">
+                            <span class="text-danger "> 100000 FCFA</span>
+                        </h6>
+                        <a href="#" class="btn btn-light btn-small mr-1 mb-2"> DETAILS</a>
+                    </div>
+                </div>
+                <!-- Card -->
+            </div>
+            <div class="col-3"> 
+                <!-- Card -->
+                <div class="card">
+                    <img class="img w-100" src="https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/13a.jpg" alt="Sample">
+                    <div class="card-body">
+                        <h5 class="text-center"> Artice x </h5>
+                        <p class="small text-muted text-uppercase text-center"> 01 disponible</p>
+                        <hr>
+                        <h6 class="mb-3 text-center">
+                            <span class="text-danger "> 100000 FCFA</span>
+                        </h6>
+                        <a href="#" class="btn btn-light btn-small mr-1 mb-2"> DETAILS</a>
+                    </div>
+                </div>
+                <!-- Card -->
+            </div>
+            <div class="col-3"> 
+                <div class="card">
+                    <img class="img w-100" src="https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/13a.jpg" alt="Sample">
+                    <div class="card-body">
+                        <h5 class="text-center"> Artice x </h5>
+                        <p class="small text-muted text-uppercase text-center"> 01 disponible</p>
+                        <hr>
+                        <h6 class="mb-3 text-center">
+                            <span class="text-danger "> 100000 FCFA</span>
+                        </h6>
+                        <a href="#" class="btn btn-light btn-small mr-1 mb-2"> DETAILS</a>
+                    </div>
+                </div>
+            </div>
         </div>
-            
-    
+
+        <div class="container row m-3" style="background-color:red"> 
+            <div class="col-12">
+                <div class="text-center"> 
+                     1 2 3 
+                </div>
+            </div>
+        </div>  
     </div>
     
 <?php 
