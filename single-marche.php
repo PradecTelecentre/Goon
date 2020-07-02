900caf641658c4895d0e1661076d1103d098f669
Abdoul aziz Hamayadji <abdoulazizhamayadji95@gmail.com>
2020-07-02 11:50:13 +0100

    single pages modifications

--- single-marche.php
+++ single-marche.php
@@ -55,7 +55,7 @@ get_header();
                             <div class="col-md-6 col-lg-6 col-xs-12 col-sm-12">
                                 <h5 style="font-weight: 500;"> <?php the_title(); the_field('article_nom') ; ?> </h5>
                                 <span class="mr-1">
-                                    <strong class="shadow-sm bg-danger" style="font-weight: 600; color: #4f4f4f;"> <?php the_field('article_prix') ?> FCFA</strong> 
+                                    <strong class="shadow-sm" style="font-weight: 600; color: orange;"> <?php the_field('article_prix') ?> FCFA</strong> 
                                 </span>
                                 <p class="small text-muted  mb-2">Description</p>
                                 <p class="pt-1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, sapiente illo. Sit
@@ -64,9 +64,9 @@ get_header();
                                 </p>
                                 <hr>
                                 <p class="small text-muted  mb-2">Personne à contacter</p>
-                                <div class="row text-center" style="">
-                                    <div type="" class="bg-primary btn-sm mr-1 mb-2 col-md-6 col-lg-6 col-xs-12 col-sm-12" style="font-size:25px; font-weight:600"><?php the_field('article_proprietaire') ?> </div>
-                                    <div type="" class="bg-danger btn-sm px-3 mb-2 col-md-6 col-lg-6 col-xs-12 col-sm-12" style="font-size:30px; font-weight:600"> +237 <?php the_field('article_numero') ?></div>
+                                <div class="row text-center " style="">
+                                    <div type="" class=" btn-sm  mb-2 col-md-6 col-lg-6 col-xs-12 col-sm-12" style="font-size:25px; font-weight:600"><?php the_field('article_proprietaire') ?> </div>
+                                    <div type="" class=" btn-sm px-3 mb-2 col-md-6 col-lg-6 col-xs-12 col-sm-12" style="font-size:30px; font-weight:600"> +237 <?php the_field('article_numero') ?></div>
                                 </div>
                             </div>
                         <?php  endwhile; // End the loop. ?>
