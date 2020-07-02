3f6bb006ba32ae89a5859b995d568b1f4f59c9e8
Abdoul aziz Hamayadji <abdoulazizhamayadji95@gmail.com>
2020-07-02 12:11:26 +0100

    single pages modifications

--- page-addarticle.php
+++ page-addarticle.php
@@ -17,7 +17,7 @@ get_header();
 
     <div class="container shadow " style=" margin-top:-65px; "> 
         <div class="container row text-center" style="position:absolute; background-color: whitesmoke;"> 
-            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center"> 
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center mb-4 pb-5 submitbutton"> 
                     <?php
                     $new_post= array(
                                 'post_id' => 'new_post', // Unique identifier for the form
@@ -46,6 +46,13 @@ get_header();
                     acf_form( $new_post); ?>
             </div>
         </div>
-</div>  
+    </div> 
+     <style>
+        .acf-form-submit input{
+            background-color: orange;
+            font-size: 25px;
+            font-weight:600;
+        } 
+     </style>               
 <?php
 get_footer();
