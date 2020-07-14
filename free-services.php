7a222026742a56a31be5a4c8bc0774ef8566d0d4
Abdoul aziz Hamayadji <abdoulazizhamayadji95@gmail.com>
2020-07-14 03:54:11 +0100

    espace citoyen link

--- free-services.php
+++ free-services.php
@@ -66,7 +66,7 @@
 
 
                     <div class="col-md-3 mb-4 col-sm-6 col-lg-3 mobile-cover">
-                        <a href="http://marche.mandama.cm/" rel="bookmark">
+                        <a href="http://goon.mandama.cm/marches" rel="bookmark">
                             <div class="p-2 bg-white style-div-md-4">
                                 <img src="<?= get_template_directory_uri() . '/src/img/v2/marche.png' ?>" alt="">
                                 <?php if (strpos(home_url('/'), 'lang=en') !== false) { ?>
