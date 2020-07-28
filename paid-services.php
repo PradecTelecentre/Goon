b3c2f0a017bd5d7553b89c0601bea79369ba5df2
claudelkros <desiretaira@gmail.com>
2020-07-28 08:56:08 +0100

    Changes made on raspbian

--- paid-services.php
+++ paid-services.php
@@ -12,9 +12,9 @@
                     <div class="free col paid-content-remove-spac">
                         <a href="#" rel="bookmark" style="background: #fff !important">
                             <div class="p-2 style-div-md-4 bg-white" style="background: #fff !important">
-                                <img src="<?= get_template_directory_uri() . '/src/img/v2/net.png' ?>" alt="" style="background: #fff !important">
+                                <img src="<? get_template_directory_uri() . '/src/img/v2/net.png' ?>" alt="" style="background: #fff !important">
                                 <div class="arrow" style="max-width: 201px; margin-right:auto; margin-left:auto;margin-top: -5px">
-                                    <img src="<?= get_template_directory_uri() . '/src/img/cadre.svg'  ?>" alt="" style="margin-top: -33px; width: 100px">
+                                    <img src="<? get_template_directory_uri() . '/src/img/cadre.svg'  ?>" alt="" style="margin-top: -33px; width: 100px">
                                 </div>
                                 <?php if (strpos(home_url('/'), 'lang=en') !== false) { ?>
                                     <h6 class="bottom-left" style="margin-top:-38px">INTERNET</h6>
@@ -23,19 +23,19 @@
                                 <?php } ?>
 
                                 <div class="descript" style="height: 80%; max-width: auto;">
-                                    <p>Internet c'est avoir toute la sagesse et toute la bassesse du monde au bout des doigts.</p>
-                                </div>
+                                      </div>
                             </div>
-                        </a>
+                                   <p>Internet c'est avoir toute la sagesse et toute la bassesse du monde au bout des doigts.</p>
+                   </a>
                     </div>
 
 
                     <div class="free col">
                         <a href="http://owncloud.goon.cm/index.php/apps/files/?dir=/&fileid=3" rel="bookmark" style="background: #fff !important">
                             <div class="p-2 style-div-md-4 bg-white" style="background: #fff !important">
-                                <img src="<?= get_template_directory_uri() . '/src/img/v2/espace.png' ?>" alt="" style="background: #fff !important">
+                                <img src="<? get_template_directory_uri() . '/src/img/v2/espace.png' ?>" alt="" style="background: #fff !important">
                                 <div class="arrow" style="max-width: 201px; margin-right:auto; margin-left:auto;margin-top: -5px">
-                                    <img src="<?= get_template_directory_uri() . '/src/img/cadre.svg'  ?>" alt="" style="margin-top: -33px; width: 100px">
+                                    <img src="<? get_template_directory_uri() . '/src/img/cadre.svg'  ?>" alt="" style="margin-top: -33px; width: 100px">
                                 </div>
 
                                 <?php if (strpos(home_url('/'), 'lang=en') !== false) { ?>
@@ -54,9 +54,9 @@
                     <div class="free col">
                         <a href="#" rel="bookmark" style="background: #fff !important">
                             <div class="p-2 style-div-md-4 bg-white" style="background: #fff !important">
-                                <img src="<?= get_template_directory_uri() . '/src/img/v2/apps.png' ?>" alt="" style="background: #fff !important">
+                                <img src="<? get_template_directory_uri() . '/src/img/v2/apps.png' ?>" alt="" style="background: #fff !important">
                                 <div class="arrow" style="max-width: 201px; margin-right:auto; margin-left:auto;margin-top: -5px">
-                                    <img src="<?= get_template_directory_uri() . '/src/img/cadre.svg'  ?>" alt="" style="margin-top: -33px; width: 100px">
+                                    <img src="<? get_template_directory_uri() . '/src/img/cadre.svg'  ?>" alt="" style="margin-top: -33px; width: 100px">
                                 </div>
                                 <?php if (strpos(home_url('/'), 'lang=en') !== false) { ?>
                                     <h6 class="bottom-left" style="margin-top:-38px">APPLICATIONS</h6>
