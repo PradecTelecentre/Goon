a255c9d32b728e51cf62c38b101aebc69e01a4e4
Abdoul aziz Hamayadji <abdoulazizhamayadji95@gmail.com>
2020-10-20 16:08:58 +0100

    add footer to demo page

--- services-template.php
+++ services-template.php
@@ -11,28 +11,31 @@
 
 get_header();
 ?>
-  <div id="video_presentation" class="modal fade mr-auto ml-auto p-0">
+<div id="video_presentation" class="modal fade mr-auto ml-auto p-0">
 
-        <div class="modal-dialog">
+    <div class="modal-dialog">
 
-            <div class="modal-content bg-dark only__desktop">
+        <div class="modal-content bg-dark only__desktop">
 
-                <div class="modal-header text-center">
-                    <button type="button" class="text-white bg-danger" data-dismiss="modal" aria-hidden="true" onclick="myClose()">&times;</button>
-                    <span class="modal-title text-white" style="font-size: 1.2rem; text-transform: uppercase;"><?php _e('Video Presentation of GOON','wpml_theme'); ?></span>
-                </div>
+            <div class="modal-header text-center">
+                <button type="button" class="text-white bg-danger" data-dismiss="modal" aria-hidden="true"
+                    onclick="myClose()">&times;</button>
+                <span class="modal-title text-white"
+                    style="font-size: 1.2rem; text-transform: uppercase;"><?php _e('Video Presentation of GOON','wpml_theme'); ?></span>
+            </div>
 
-                <div class="modal-body mr-auto ml-auto">
-                    <video controls  autoplay="true" height="100%" width="100%" id="sound">
-                        <source src="<?php echo get_template_directory_uri() ?>/src/video/presentations.mp4" type="video/mp4">
-                    </video>
-                </div>
-
+            <div class="modal-body mr-auto ml-auto">
+                <video controls autoplay="true" height="100%" width="100%" id="sound">
+                    <source src="<?php echo get_template_directory_uri() ?>/src/video/presentations.mp4"
+                        type="video/mp4">
+                </video>
             </div>
 
         </div>
 
     </div>
+
+</div>
 <?php
 //  include des services payants
 include "paid-services.php";
@@ -45,4 +48,4 @@ include "free-services.php";
 
 <?php
 
-// get_footer();
+get_footer();
