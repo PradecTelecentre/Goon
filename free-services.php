ff6dd0e8135dc1763e5ba0168487a44e7d14e8a1
Abdoul aziz Hamayadji <abdoulazizhamayadji95@gmail.com>
2020-10-26 19:52:37 +0100

    rocket chat link added

--- free-services.php
+++ free-services.php
@@ -80,7 +80,7 @@
                     </div>
 
                     <div class="col-md-3 mb-4 col-sm-6 col-lg-3 mobile-cover">
-                        <a href="http://www.goon.cm:5000/login" rel="bookmark" target="_blank">
+                        <a href="http://goon.africa:3000/channel/general" rel="bookmark" target="_blank">
                             <div class="p-2 bg-white style-div-md-4">
                                 <img src="<?= get_template_directory_uri() . '/src/img/v2/messag.png' ?>" alt="">
                                 <?php if (strpos(get_page_link(), 'lang=en') !== false) { ?>
