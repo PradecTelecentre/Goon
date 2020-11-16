8d08b37e7855c30f4421e03b342a693e69116167
Abdoul aziz Hamayadji <abdoulazizhamayadji95@gmail.com>
2020-11-16 09:37:51 +0100

    garoua 2 divertissement branch

--- free-services.php
+++ free-services.php
@@ -180,8 +180,7 @@
                     </div>
 
                     <div class="col-md-3 mb-4 col-sm-6 col-lg-3 mobile-cover">
-                        <a href="http://owncloud.goon.cm" rel="bookmark" data-toggle="modal"
-                            data-target="#construction">
+                        <a href="http://www.goon.cm/divertissement/" rel="bookmark">
                             <div class="style-div-md-4 p-2 bg-white">
                                 <img src="<?= get_template_directory_uri() . '/src/img/v2/games.png' ?>" alt="">
                                 <?php if (strpos(get_page_link(), 'lang=en') !== false) { ?>
