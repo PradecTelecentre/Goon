97a076c6b3fc1668b02a4282bebcdc5661a2938a
Abdoul aziz Hamayadji <abdoulazizhamayadji95@gmail.com>
2021-10-20 10:19:32 +0100

    single page margin

--- single.php
+++ single.php
@@ -13,35 +13,38 @@ get_header();
 
 <div id="primary" class="content-area">
     <!--<main id="main" class="site-main">-->
-        <div class="container-fluid" style="margin-top: 100px; ">
-            <div class="row" style="margin-left: 1rem;margin-right:2rem;">
-                <div class="col-md-4" style="margin-top:4rem;">
-                    <hr class="hr-news">
-                    <?php if (strpos(home_url('/'), 'lang=en') !== false) { ?>
-                        <h4 class="news-title-h2 p-1">COMMUNITY NEWS</h4>
-                    <?php } else { ?>
-                        <h5 class="news-title-h2 p-1">LES NOUVELLES LOCALES</h5>
-                    <?php }
+    <div class="container-fluid" style="margin-top: 300px; ">
+        <div class="row" style="margin-left: 1rem;margin-right:2rem;">
+            <div class="col-md-4" style="margin-top:4rem;">
+                <hr class="hr-news">
+                <?php if (strpos(home_url('/'), 'lang=en') !== false) { ?>
+                <h4 class="news-title-h2 p-1">COMMUNITY NEWS</h4>
+                <?php } else { ?>
+                <h5 class="news-title-h2 p-1">LES NOUVELLES LOCALES</h5>
+                <?php }
                       while (have_posts()):
                       the_post();
                       if (has_post_thumbnail()) { ?>
-                        <div class="t-center"><?php echo get_the_post_thumbnail();?></div>
-                      <?php } else { ?>
-                        <img src="<?php echo get_theme_file_uri("onews.png");?>" alt="activities">
-                    <?php  };
+                <div class="t-center"><?php echo get_the_post_thumbnail();?></div>
+                <?php } else { ?>
+                <img src="<?php echo get_theme_file_uri("onews.png");?>" alt="activities">
+                <?php  };
                     ?>
-                </div>
-                    <div class="col-md-8" style="margin-top: 2rem;">
-                      <div class="generic-content">
-                        <h2 class="headline headline--medium headline--post-title event-summary__title--com"><a href="<?php the_permalink();?>"><?php the_title();?></a></h2>
-                            <div class="metabox metabox--com">
-                              <p><a class="metabox__blog-home-link meta--com" href="<?php echo site_url('/news');?>"><?php _e('News Page','wpml_theme'); ?></a><span class="metabox__main"></span><?php _e('Posted by ','wpml_theme');?>
-                                 <?php the_author_posts_link();?>
-                                 <?php _e(' on ','wpml_theme');?>
-                                 <?php the_time('d/m/Y');?>
-                                 <?php _e(' in ','wpml_theme');?> <?php echo get_the_category_list('and')?> </p>
-                            </div>
-                          <?php  the_content();
+            </div>
+            <div class="col-md-8" style="margin-top: 2rem;">
+                <div class="generic-content">
+                    <h2 class="headline headline--medium headline--post-title event-summary__title--com"><a
+                            href="<?php the_permalink();?>"><?php the_title();?></a></h2>
+                    <div class="metabox metabox--com">
+                        <p><a class="metabox__blog-home-link meta--com"
+                                href="<?php echo site_url('/news');?>"><?php _e('News Page','wpml_theme'); ?></a><span
+                                class="metabox__main"></span><?php _e('Posted by ','wpml_theme');?>
+                            <?php the_author_posts_link();?>
+                            <?php _e(' on ','wpml_theme');?>
+                            <?php the_time('d/m/Y');?>
+                            <?php _e(' in ','wpml_theme');?> <?php echo get_the_category_list('and')?> </p>
+                    </div>
+                    <?php  the_content();
 
                             //the_post_navigation();
 
@@ -51,12 +54,12 @@ get_header();
                             // endif;
                             ?>
 
-                      </div>
-                    </div>
-                <?php endwhile; // End of the loop.
-                ?>
+                </div>
             </div>
+            <?php endwhile; // End of the loop.
+                ?>
         </div>
+    </div>
     <!-- #main -->
 </div><!-- #primary -->
 
